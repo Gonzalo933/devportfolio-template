21c7758eae43815369fd5b0851136dc8f79ef170
Gonzalo933 <gonzalo.hernandez.1293@gmail.com>
2017-07-26 17:43:02 +0200

    actualizado blog

--- blog/pelicanconf.py
+++ blog/pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Gonzalo Hernandez'
 SITENAME = 'Machine Learning Blog'
-SITEURL = ''
+SITEURL = 'http://gonzalohernandezmunoz.com/blog/output'
 
 PATH = 'content'
 
